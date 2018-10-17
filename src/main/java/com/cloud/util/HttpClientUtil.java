dca4a4200f4c7610bd301b0acef6bdeaeddf8068
1142266124@qq.com <1142266124@qq.com>
2018-10-17 09:26:15 +0800

    config file

--- src/main/java/com/cloud/util/HttpClientUtil.java
+++ src/main/java/com/cloud/util/HttpClientUtil.java
@@ -80,6 +80,7 @@ public class HttpClientUtil {
         headers.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
         HttpResult httpResult = doPostJSONHeaders(url,json,headers,HttpRequestConfig.defaultConfig());
         System.out.println(httpResult.toString());
+
     }
 
     /**
